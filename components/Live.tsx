38ebcded5fdb79ce7709c5d80873ece65209daba
hazhir <hazhir6690@gmail.com>
2024-05-15 19:32:08 +0330

    feat: added reaction in live cursor

--- components/Live.tsx
+++ components/Live.tsx
@@ -1,21 +1,46 @@
 import React, { useCallback, useEffect, useState } from "react";
 import LiveCursor from "@/components/cursor/LiveCursor";
-import { useMyPresence, useOthers } from "@/liveblocks.config";
+import { useBroadcastEvent, useEventListener, useMyPresence, useOthers } from "@/liveblocks.config";
 import CursorChat from "@/components/cursor/CursorChat";
-import { CursorMode } from "@/types/type";
+import { CursorMode, CursorState, Reaction, ReactionEvent } from "@/types/type";
+import ReactionSelector from "@/components/reaction/ReactionButton";
+import FlyingReaction from "@/components/reaction/FlyingReaction";
+import useInterval from "@/hooks/useInterval";
 
 const Live = () => {
+  const [reactions, setReactions] = useState<Reaction[]>([]);
   const others = useOthers();
   const [{ cursor }, updateMyPresence] = useMyPresence() as any;
-  const [cursorState, setCursorState] = useState({
+  const [cursorState, setCursorState] = useState<CursorState>({
     mode: CursorMode.Hidden,
   });
+
+  const broadcast = useBroadcastEvent();
+  useInterval(() => {
+    if (cursorState.mode === CursorMode.Reaction && cursorState.isPressed && cursor) {
+      setReactions((reactions) => reactions.concat({
+        point: { x: cursor.x, y: cursor.y },
+        value: cursorState.reaction,
+        timestamp: Date.now(),
+      }));
+
+      broadcast({
+        x: cursor.x,
+        y: cursor.y,
+        value: cursorState.reaction,
+      });
+    }
+  }, 100);
+
   const handelPointerMove = useCallback((event: React.PointerEvent) => {
     event.preventDefault();
-    const x = event.clientX - event.currentTarget.getBoundingClientRect().x;
-    const y = event.clientY - event.currentTarget.getBoundingClientRect().y;
 
-    updateMyPresence({ cursor: { x, y } });
+    if (cursor === null || cursorState.mode !== CursorMode.ReactionSelector) {
+
+      const x = event.clientX - event.currentTarget.getBoundingClientRect().x;
+      const y = event.clientY - event.currentTarget.getBoundingClientRect().y;
+      updateMyPresence({ cursor: { x, y } });
+    }
   }, []);
 
   const handelPointerLeave = useCallback((event: React.PointerEvent) => {
@@ -30,18 +55,43 @@ const Live = () => {
     const y = event.clientY - event.currentTarget.getBoundingClientRect().y;
 
     updateMyPresence({ cursor: { x, y } });
+
+    setCursorState((state: CursorState) => cursorState.mode === CursorMode.Reaction ? {
+        ...state,
+        isPressed: true,
+      } : state,
+    );
+  }, [cursorState.mode, setCursorState]);
+
+  const handlePointerUp = useCallback((event: React.PointerEvent) => {
+    setCursorState((state: CursorState) => cursorState.mode === CursorMode.Reaction ? {
+      ...state,
+      isPressed: true,
+    } : state);
+  }, [cursorState.mode, setCursorState]);
+
+  const setReaction = useCallback((reaction: string) => {
+    setCursorState({ mode: CursorMode.Reaction, reaction, isPressed: false });
   }, []);
+
   useEffect(() => {
     const onKeyUp = (e: KeyboardEvent) => {
       if (e.key === "/") {
+
         setCursorState({
           mode: CursorMode.Chat,
+          // @ts-ignore
           previousMessage: null,
           message: "",
         });
       } else if (e.key === "Escape") {
         updateMyPresence({ message: "" });
         setCursorState({ mode: CursorMode.Hidden });
+      } else if (e.key === "e") {
+        setCursorState({
+          mode: CursorMode.ReactionSelector,
+
+        });
       }
 
     };
@@ -50,30 +100,60 @@ const Live = () => {
         e.preventDefault();
       }
     };
-    window.addEventListener("keyup",onKeyUp)
-    window.addEventListener("keydown",onKeyDown)
+    window.addEventListener("keyup", onKeyUp);
+    window.addEventListener("keydown", onKeyDown);
 
-    return ()=>{
-      window.removeEventListener("keyup",onKeyUp);
-      window.removeEventListener("keydown",onKeyDown);
-    }
+    return () => {
+      window.removeEventListener("keyup", onKeyUp);
+      window.removeEventListener("keydown", onKeyDown);
+    };
   }, [updateMyPresence]);
+  useEventListener((eventData) => {
+    const event = eventData.event as ReactionEvent;
+    setReactions((reactions) => reactions.concat({
+      point: { x: event.x, y: event.y },
+      value: event.value,
+      timestamp: Date.now(),
+    }));
+  });
   return (
     <div
       onPointerMove={handelPointerMove}
       onPointerDown={handelPointerDown}
+      onPointerUp={handlePointerUp}
       onPointerLeave={handelPointerLeave}
       className="w-full h-[100vh] text-center flex justify-center items-center"
     >
       <h1 className="text-2xl text-white "> LiveBlocks Figma clone</h1>
+
+      {
+        reactions.map((reaction) => (
+          <FlyingReaction
+            key={reaction.timestamp.toString()}
+            x={reaction.point.x}
+            y={reaction.point.y}
+            timestamp={reaction.timestamp}
+            value={reaction.value}
+          />
+        ))
+      }
+
       {cursor && (
         <CursorChat
           cursor={cursor}
+          // @ts-ignore
           cursorState={cursorState}
           setCursorState={setCursorState}
           updateMyPresence={updateMyPresence}
         />
       )}
+
+      {cursorState.mode === CursorMode.ReactionSelector && (
+        <ReactionSelector
+          setReaction={setReaction}
+        />
+      )}
+
       <LiveCursor others={others} />
     </div>
   );
--- components/cursor/Cursor.tsx
+++ components/cursor/Cursor.tsx
@@ -10,9 +10,13 @@ const Cursor = ({ color, x, y, message }: Props) => {
   return (
     <div className="pointer-events-none absolute top-0 left-0"
     style={{transform:`translateX(${x}px) translateY(${y}px)`,}}>
-      <CursorSVG color={color}/>
+      <CursorSVG
+        // @ts-ignore
+        color={color}
+      />
 
       {message&&(
+        // @ts-ignore
         <div className="absolute left-0 top-5 rounded-3xl px-4 py-2 " style={{backgroundColor:color}}>
           <p className="text-white whitespace-nowrap text-sm leading-relaxed">{message}</p>
         </div>
