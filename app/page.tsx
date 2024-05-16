c737d88df3cbe738e68467423ea1ef60e9f686b9
hazhir <hazhir6690@gmail.com>
2024-05-16 18:54:10 +0330

    feature: using facric.js lib and drawing rectangle with canvas in fabric

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,46 @@
 "use client";
+import { fabric } from "fabric";
 import Live from "@/components/Live";
 import Navbar from "@/components/Navbar";
+import LeftSideBar from "@/components/LeftSideBar";
+import RightSideBar from "@/components/RightSideBar";
+import { useEffect, useRef } from "react";
+import { handleCanvasMouseDown, handleResize, initializeFabric } from "@/lib/canvas";
 
 export default function Page() {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const fabricRef = useRef<fabric.canvas|null>(null);
+  const isDrawing = useRef(false);
+
+  const shapeRef = useRef<fabric.object | null>(null);
+  const selectedShapeRef = useRef<string | null>("rectangle");
+
+  useEffect(() => {
+    const canvas = initializeFabric({ canvasRef, fabricRef });
+
+    canvas.on("mouse:down", (options) => {
+      console.log(77);
+      handleCanvasMouseDown(
+        {
+          options,
+          canvas,
+          isDrawing,
+          shapeRef,
+          selectedShapeRef,
+        });
+    });
+    window.addEventListener("resize",()=>{
+      // handleResize({fabricRef})
+    })
+  }, []);
   return (
     <main className="h-screen overflow-hidden ">
       <Navbar />
       <section className="flex h-full flex-row">
-        <Live />
+        <LeftSideBar />
+        <Live canvasRef={canvasRef}/>
+        <RightSideBar />
+
       </section>
 
     </main>
